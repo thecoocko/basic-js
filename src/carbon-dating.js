9cb380e55be4a3e4fd0ae51ba2c8c4f3fa3da849
coocko <krabik4794@gmail.com>
2021-03-04 14:14:40 +0200

    feat:add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,14 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const T_HALF = 0.693;
 
 module.exports = function dateSample(sampleActivity) {
- 
+    let k,t;
+     if(typeof(sampleActivity) == 'string' && sampleActivity.length !== 0){
+        k = T_HALF/HALF_LIFE_PERIOD;
+        t = Math.ceil(Math.log(MODERN_ACTIVITY/Number(sampleActivity))/k);
+        if(t > 0 && Number.isInteger(t)) return t;
+        else return false;
+    }else return false;
 };
