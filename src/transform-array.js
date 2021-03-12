e732743f7735c6688e9698864ac376a06e99d755
Coocko <krabik4794@gmail.com>
2021-03-12 19:32:44 +0200

    feat: adsd

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+const actions = ['--discard-next','--discard-prev', '--double-next','--double-prev'];
+
+for(let i = 0 ; i< arr.length; i++){
+    
+    if(arr[i] === actions[0]){
+        arr.splice(i,1);
+        arr.splice(i+1,1);
+    }
+
+    if(arr[i] === actions[1]){
+        arr.splice(i,1);
+        arr.splice(i-1,1);
+    }
+    
+    if(arr[i] === actions[2]){
+        arr.splice();
+    }
+
+    if(arr[i] === actions[3]){
+        arr.splice();
+    }
+    
+}
+  return arr;
 };
