a3f021452e92a58fc25d17dbf259bec9840abc64
Coocko <krabik4794@gmail.com>
2021-03-12 22:07:34 +0200

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,29 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-
 const actions = ['--discard-next','--discard-prev', '--double-next','--double-prev'];
-
-for(let i = 0 ; i< arr.length; i++){
-    
-    if(arr[i] === actions[0]){
-        arr.splice(i,1);
-        arr.splice(i+1,1);
-    }
-
-    if(arr[i] === actions[1]){
-        arr.splice(i,1);
-        arr.splice(i-1,1);
-    }
-    
-    if(arr[i] === actions[2]){
-        arr.splice();
-    }
-
-    if(arr[i] === actions[3]){
-        arr.splice();
-    }
-    
-}
+let tmp = arr;
+        for(let i = 0 ; i< tmp.length; i++){
+            if(arr[i] === actions[0]){
+                arr.splice(i+1,1);
+                arr.splice(i,1);
+            }
+            if(arr[i] === actions[1]){
+                arr.splice(i-1,1);
+                arr.splice(i,1);
+            }
+            if(arr[i] === actions[2]){
+                arr.splice(i,0,arr[i+1]);
+                arr.splice(i,1);
+            }
+            if(arr[i] === actions[3]){
+                arr.splice(i,0,arr[i-1]);
+                arr.splice(i,1);
+            }
+        }
   return arr;
 };
