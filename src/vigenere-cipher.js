630f980ca92cfcc726f075ece6dc5003f7babec7
Coocko <krabik4794@gmail.com>
2021-03-15 06:47:44 +0200

    feat:add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,70 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message,key) {
+    
+      //split the string into an array
+      let arr = message.split("");
+      let keyValue = [];
+
+      for (let i = 0; i < key.length; i++) {
+          //finding the letter(key) value and push it to key value
+          keyValue.push(arr.indexOf(key[i]));
+      }
+      let redacted = "";
+      let pointer = 0;
+      for (let i = 0; i < message.length; i++) {
+          if (arr.includes(message[i])) {
+              let shift = arr.indexOf(message[i]) + keyValue[pointer];
+
+              if (shift >= 26) {
+                  redacted += arr[shift - 26];
+              } else {
+                  redacted += arr[shift];
+              }  
+              pointer += 1;
+          } else {
+              pointer = 0;
+              redacted += message[i];
+          }
+
+          if (pointer >= keyValue.length) {
+              pointer = 0;
+          }
+      }
+      return redacted
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message,key) {
+      let arr = message.split("");
+      let keyValue = [];
+
+      for (let i = 0; i < key.length; i++) {
+          //finding the letter(key) value and push it to key value
+          keyValue.push(arr.indexOf(key[i]));
+      }
+      let decoded = "";
+      let pointer = 0;
+      for (let i = 0; i < message.length; i++) {
+          if (arr.includes(message[i])) {
+              let shift = arr.indexOf(message[i]) - keyValue[pointer];
+              if (shift < 0) {
+                  decoded += arr[26 - Math.abs((arr.indexOf(message[i]) - keyValue[pointer]))];
+              } else {
+                  decoded += arr[Math.abs((arr.indexOf(message[i]) - keyValue[pointer]))];
+              }
+
+              pointer += 1;
+          } else {
+              pointer = 0;
+              decoded += message[i];
+          }
+
+          if (pointer >= keyValue.length) {
+              pointer = 0;
+          }
+      }
+      return decoded
   }
 }
 
